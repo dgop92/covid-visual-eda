f64e61c2553615d965b3064c91c53a1e9ded3d55
dgop92 <inevaup@gmail.com>
2022-10-09 17:55:24 -0500

    fix: logging with differents envs

--- config/logging.py
+++ config/logging.py
@@ -1,8 +1,10 @@
 import logging
 import logging.config
 
+from config.settings import LOGGING_CONFIG_FILE
+
 
 def config_logger():
-    logging.config.fileConfig(fname="logging.conf", disable_existing_loggers=False)
+    logging.config.fileConfig(fname=LOGGING_CONFIG_FILE, disable_existing_loggers=False)
     logger = logging.getLogger(__name__)
     logger.info("logger configured")
